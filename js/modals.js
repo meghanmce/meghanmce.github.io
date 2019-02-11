a631dd5ee12cd2340cbac56ae03b6b4634cdc583
Meghan McEneaney <mmceneaney@ri.pn>
2019-02-11 01:05:54 -0500

    work pg updates

---
+++ js/modals.js
@@ -0,0 +1,31 @@
+function ripenModal() {
+    document.getElementById("ripenModal").classList.add("is-active");
+}
+
+function hcModal() {
+  document.getElementById("hcModal").classList.add("is-active");
+}
+
+function ifModal() {
+  document.getElementById("ifModal").classList.add("is-active");
+}
+
+function vibesModal() {
+  document.getElementById("vibesModal").classList.add("is-active");
+}
+
+function closeripenModal() {
+  document.getElementById("ripenModal").classList.remove("is-active");
+}
+
+function closehcModal() {
+  document.getElementById("hcModal").classList.remove("is-active");
+}
+
+function closeifModal() {
+  document.getElementById("ifModal").classList.remove("is-active");
+}
+
+function closevibesModal() {
+  document.getElementById("vibesModal").classList.remove("is-active");
+}
